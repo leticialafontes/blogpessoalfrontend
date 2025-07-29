e2c3eb66c292076cce9eac3397d4ecb7a8fbf9b1
Leticia Fontes <leticia.abib95@gmail.com>
2025-07-29 18:13:57 -0300

    fix: ajuste no login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,53 +1,34 @@
-import { Link } from "react-router-dom";
-import "./style.css";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../context/UserContext";
 
 function Login() {
+  const { nome, trocarNome } = useContext(UserContext);
+  let navigate = useNavigate();
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    navigate("/home");
+  }
+
   return (
-    <>
-      <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold ">
-        <form className="flex justify-center items-center flex-col w-1/2 gap-4">
-          <h2 className="text-slate-900 text-5xl ">Entrar</h2>
-          <div className="flex flex-col w-full">
-            <label htmlFor="usuario">Usuário</label>
-            <input
-              type="text"
-              id="usuario"
-              name="usuario"
-              placeholder="Usuario"
-              className="border-2 border-slate-700 rounded p-2"
-            />
-          </div>
-          <div className="flex flex-col w-full">
-            <label htmlFor="senha">Senha</label>
-            <input
-              type="password"
-              id="senha"
-              name="senha"
-              placeholder="Senha"
-              className="border-2 border-slate-700 rounded p-2"
-            />
-          </div>
-          <button
-            type="submit"
-            className="rounded bg-indigo-400 flex justify-center
-                                   hover:bg-indigo-900 text-white w-1/2 py-2"
-          >
-            <span>Entrar</span>
-          </button>
-
-          <hr className="border-slate-800 w-full" />
-
-          <p>
-            Ainda não tem uma conta?{" "}
-            <Link to="/cadastro" className="text-indigo-800 hover:underline">
-              Cadastre-se
-            </Link>
-          
-          </p>
-        </form>
-        <div className="fundoLogin hidden lg:block"></div>
-      </div>
-    </>
+    <div className="container">
+      <form onSubmit={handleSubmit} className="formulario">
+        <h1>Autenticar Usuário</h1>
+        <div className="campo">
+          <label htmlFor="usuario">Nome</label>
+          <input
+            type="text"
+            id="usuario"
+            name="usuario"
+            placeholder="Nome do Usuário"
+            value={nome}
+            onChange={(event) => trocarNome(event.target.value)}
+          />
+        </div>
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
   );
 }
 
